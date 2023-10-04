359d15cad152ce1ebea41d396dce4061a1e57502
Charity Gamble <charlibydesign@gmail.com>
2023-10-03 19:37:50 -0600

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('src/_includes');
+    eleventyConfig.addPassthroughCopy('src/css');
+    eleventyConfig.addPassthroughCopy('src/assets');
+    eleventyConfig.addWatchTarget('src/css');
+    eleventyConfig.addCollection('projects', (collection) => {
+        return collection.getFilteredByGlob('src/projects/*.md');
+    });
+    eleventyConfig.addShortcode('year', () => {
+        return `${new Date().getFullYear()}`;
+    });
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'public',
+        },
+    };
+};
