5665f403f23df774ab5c32241553bbc03bfbe706
Charity Gamble <charlibydesign@gmail.com>
2023-10-07 19:47:47 -0600

    fixed issues with .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,59 +3,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/assets');
     eleventyConfig.addWatchTarget('src/css');
-    eleventyConfig.setBrowserSyncConfig({
-        callbacks: {
-          ready: function(err, bs) {
-    
-            bs.addMiddleware("*", (req, res) => {
-              if (!fs.existsSync(NOT_FOUND_PATH)) {
-                throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
-              }
-    
-              const content_404 = fs.readFileSync(NOT_FOUND_PATH);
-              // Add 404 http status code in request header.
-              res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-              // Provides the 404 content without redirect.
-              res.write(content_404);
-              res.end();
-            });
-          }
-        }
-      });
-      
-        const {
-          DateTime
-        } = require("luxon");
-      
-        // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-          eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-            return DateTime.fromJSDate(dateObj, {
-              zone: 'utc'
-            }).toFormat('yy-MM-dd');
-          });
-      
-          eleventyConfig.addFilter("readableDate", dateObj => {
-          return DateTime.fromJSDate(dateObj, {
-            zone: 'utc'
-          }).toFormat("MM-dd-yy");
-        });
-          
-          // Get the first `n` elements of a collection.
-      eleventyConfig.addFilter("head", (array, n) => {
-        if(!Array.isArray(array) || array.length === 0) {
-          return [];
-        }
-        if( n < 0 ) {
-          return array.slice(n);
-        }
-    
-        return array.slice(0, n);
-      });
-    
-      const fs = require("fs");
-    const NOT_FOUND_PATH = "_site/404.html";
-    
-    };
     eleventyConfig.addCollection('projects', (collection) => {
         return collection.getFilteredByGlob('src/projects/*.md');
     });
@@ -69,3 +16,55 @@ module.exports = function (eleventyConfig) {
             output: 'public',
         },
     };
+};
+eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          }
+
+          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
+  const {
+    DateTime
+  } = require("luxon");
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {
+        zone: 'utc'
+      }).toFormat('yy-MM-dd');
+    });
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat("MM-dd-yy");
+  });
+    
+    // Get the first `n` elements of a collection.
+eleventyConfig.addFilter("head", (array, n) => {
+  if(!Array.isArray(array) || array.length === 0) {
+    return [];
+  }
+  if( n < 0 ) {
+    return array.slice(n);
+  }
+
+  return array.slice(0, n);
+});
+
+const fs = require("fs");
+const NOT_FOUND_PATH = "_site/404.html";
